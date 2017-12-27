fcede065f897cdd756ff6372ae4d59516ceaa5ca
Michael Arnel <michael.arnel@gmail.com>
2017-12-27 09:45:51 -0500

    display lists from coras project

--- components/screens/Profile.js
+++ components/screens/Profile.js
@@ -1,83 +1,141 @@
-import React from 'react';
-import { Text, View, TextInput, KeyboardAvoidingView, TouchableOpacity, AsyncStorage, StyleSheet } from 'react-native';
+import React from 'react'
+import { Text, View, TouchableOpacity, StyleSheet, FlatList } from 'react-native'
 
-export default class Login extends React.Component {
+export default class Profile extends React.Component {
   
+    constructor() {
+        super();
+       
+        this.state = {
+            dataSource:  [{Title: '', Id: 0}]
+        }
+    }
+
+    componentDidMount() {
+        this.getAppLists();
+    }
     
     render() {
     return (
-    <View style={styles.container}>
-        <Text style={styles.header}> - Profile - </Text>
-        <Text>{this.getUserName()}</Text>
-        <TouchableOpacity style={styles.btn} onPress={this.logout}>
-            <Text>Log Out</Text>
-        </TouchableOpacity>
+    <View style={styles.wrapper} >
+        
+        <View style={styles.containerHeader}>
+            <Text style={styles.header}> - Project Lists - </Text>
+        </View>
+
+        <View style={styles.listContainer}>
+            <FlatList style={{ backgroundColor: 'white'}}
+            data={this.state.dataSource}
+            renderItem={({item}) => <Text style={styles.item}>{item.Title}</Text>}
+            keyExtractor={(item, index) => item.Id}
+            ItemSeparatorComponent={this.renderSeparator}
+            />
+        </View>
+       
+        <View style={styles.container}>
+            <TouchableOpacity style={styles.btn} onPress={this.logout}>
+                <Text>Log Out</Text>
+            </TouchableOpacity>
+        </View>
     </View>
-    );
+    )
   }
 
   logout = () => {
       this.props.navigation.navigate('Home')
   }
 
-  getUserName = () => {
+  getAppLists = () => {
     let self = this;
-    fetch('https://dev.coras.com/odata/Users?_=1514151757386',{
+    fetch('https://dev.coras.com/odata/Connections(\'E3CC8646-243C-4D95-BD84-67224112411D\')/Lists?$orderby=Title&$select=Title,+Id',{
         method: 'GET',
         headers: {
             'Accept': 'application/json',
-            'Content-Type': 'application/json;odata.metadata=minimal',
+            'Content-Type': 'application/json;odata',
             'AppId': 'cd57ab8b-e226-44fb-89d9-c7eb571ae864',
             'Authorization': 'Bearer ' + this.props.navigation.state.params.access_token
         },
         
     })
     .then((response) => response.json())
-    .then((res) => {
-        alert(res)
-        //if (res.error){
-        //    alert('Error!!!')
-        //    return null
-       // }
-        //else {
-        //  return res.DisplayName
-        //} 
-        return ''
-    })
+    .then((responseJson) => {
+        console.log(responseJson)
+        self.setState({
+          //isLoading: false,
+          dataSource: responseJson.value,
+        }, function() {
+          // do something with new state
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+      })
     .done(() => {
-        self.setState({animating: false})
+        
     })
   }
+
+  renderSeparator = () => {
+    return (
+      <View
+        style={{
+          height: 1,
+          backgroundColor: "#CED0CE",
+        }}
+      />
+    );
+  };
 }
 
 const styles = StyleSheet.create({
     wrapper: {
        flex: 1, 
+       flexDirection: 'column',
+       flexWrap: 'wrap',
+       backgroundColor: 'white',
+       alignContent: 'center'
     },
     container: {
         flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#2896d3',
+        flexDirection: 'column',
+        backgroundColor: '#fff',
         paddingLeft: 40,
         paddingRight: 40,
+        alignContent: 'center',
+        paddingTop: 15
+ 
+    },
+    containerHeader: {
+        flex: 1,
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        paddingLeft: 40,
+        paddingRight: 40,
+        alignItems: 'center',
+        top: 40
+    },
+    listContainer: {
+        flex: 5,
+        flexDirection: 'column',
+        backgroundColor: '#fff',
+        alignSelf: 'stretch',
     },
     header: {
         fontSize: 24,
         marginBottom: 60,
-        color: '#fff',
-    },
-    textInput: {
-        alignSelf: 'stretch',
-        padding: 16,
-        marginBottom: 20,
-        backgroundColor: '#fff',
+        color: 'black',
     },
     btn: {
         alignSelf: 'stretch',
         backgroundColor: '#01c853',
         padding: 20,
         alignItems: 'center',
+    },
+    item: {
+        fontSize: 21,
+        height: 50,
+        paddingLeft: 20,
+        paddingTop: 10
     }
 })
 
